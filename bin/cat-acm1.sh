da7d893c7886458147f761585ca4bba945618a15
Serge Voil <serge0x76@gmail.com>
2014-08-11 23:40:36 -0400

    fix build scripts

--- bin/cat-acm1.sh
+++ bin/cat-acm1.sh
@@ -7,7 +7,7 @@ echo i > /dev/ttyACM1
 
 while [[ 1 ]] ; do
 	date +'===== %Y-%m-%d-%H:%M:%S =======' | tee -a ttyACM1.log
-	cat /dev/ttyACM1 | tee -a ttyACM1.log | ${DIR}/../mc/tx4/filter.py
+	cat /dev/ttyACM1 | tee -a ttyACM1.log | ${DIR}/../mc/rx-hub/filter.py
 	echo restarting...
 	sleep 5
 done
